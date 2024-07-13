be5617a4b1b4dbba1246e5d021ce4eeb8bf07d53
zsh <1021018543@qq.com>
2024-07-13 11:56:10 -0400

    refined new dataset class

--- dataset_snapshot.py
+++ dataset_snapshot.py
@@ -198,25 +198,18 @@ class ExploreDataset(Dataset):
         tokenizer,
         max_length,
         scene_token=SCENE_TOKEN,
-        # frontier_token = FRONTIER_TOKEN,
         select_token=SELECT_TOKEN,
         egocentric_views=False,
         action_memory=False,
         prefiltering=False,
         random_permute=False,
         add_positional_encodings=False,
-        # Jiachen TODO: add your parameter here
         top_k_categories=5,
         num_egocentric_views=5,
         split="train",
     ):
-        # scene_path = "/gpfs/u/home/LMCG/LMCGnngn/scratch/multisensory"
         self.scene_dir = os.path.join(scene_path, "scene_feature_dict_merged_snapshots")
-        # print(self.scene_dir)
         self.ranking_path = os.path.join(scene_path, "selected_candidates.json")
-        # exploration_path = (
-        #     "/gpfs/u/home/LMCG/LMCGnngn/scratch/yanghan/3d/explore-eqa-test/"
-        # )
         self.obj_bbox_dir = "/gpfs/u/home/LMCG/LMCGnngn/scratch/multisensory/MLLM/data/hm3d/hm3d_obj_bbox_merged"
         self.explore_dir = os.path.join(exploration_path, "exploration_data_new_new")
         self.tokenizer = tokenizer
@@ -229,10 +222,6 @@ class ExploreDataset(Dataset):
         self.num_egocentric_views = num_egocentric_views
         self.top_k_categories = top_k_categories
 
-        # self.frontier_token = frontier_token
-        # self.frontier_token_id = self.tokenizer.convert_tokens_to_ids(self.frontier_token)
-        # self.select_token = select_token
-        # self.select_token_id = self.tokenizer(select_token).input_ids[-1]
         self.max_length = max_length
         self.split = split
         self.data = self.load_data()
@@ -266,17 +255,9 @@ class ExploreDataset(Dataset):
         )
         frontier_folder = os.path.join(epi_path, "frontier_rgb")
         for frontier in stepdata["frontiers"]:
-            # placeholder for loading frontier feature
             rgb_id = frontier["rgb_id"]
-            # load frontier feature
-            # feature = torch.load(os.path.join(frontier_folder, rgb_id.replace(".png", ".pt")),
-            #                         map_location = 'cpu')
             feature = os.path.join(frontier_folder, rgb_id.replace(".png", ".pt"))
-            # feature = torch.zeros(1024)
             stepdata["frontier_features"][rgb_id] = feature
-            # front['rgb_id'] = os.path.join(epi_path,'frontier_rgb',front['rgb_id'])
-        # remove frontier info, can be removed in case other features needed
-        # del stepdata['frontiers']
         stepdata["snapshot_features"] = {}
         stepdata["snapshot_objects"] = {}
         snapshot_folder = os.path.join(epi_path, "object_features")
@@ -301,34 +282,15 @@ class ExploreDataset(Dataset):
         return stepdata
 
     def load_data(self):
-
-        # Jiachen TODO: load your "question/scene to ranking json" here
-
         # load scene feature into dict
         with open(self.ranking_path, "r") as f:
             self.candidate_rankings = json.load(f)
-        # self.scenes = {}
-        # for scene in os.listdir(self.scene_dir):
-        #     self.scenes[scene] = {}
-        #     scene_fold = os.path.join(self.scene_dir, scene)
-        #     # need to confirm: if object in different scene should have different features
-        #     for object_f in os.listdir(scene_fold):
-        #         object_id = object_f[:-3]
-        #         try:
-        #             # object_feature  = torch.load(os.path.join(scene_fold, object_f),
-        #             #                             map_location = 'cpu')
-        #             # self.scenes[scene][object_id] = object_feature
-        #             self.scenes[scene][object_id] = os.path.join(scene_fold, object_f)
-        #         except:
-        #             continue
 
         self.obj_json_map = {}
         for obj_json in os.listdir(self.obj_bbox_dir):
             scene_id = obj_json.split(".")[0]
             self.obj_json_map[scene_id] = os.path.join(self.obj_bbox_dir, obj_json)
 
-        # load episode data: metadata is managed with self.episodes
-        # TODO later: Remove num skipped to improve error handling
         self.episodes = []
         data = []
         self.episode2step = defaultdict(list)
@@ -361,19 +323,6 @@ class ExploreDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-
-        # Jiachen TODO: add your feature to get item
-        # which might include the following steps
-        # 1. load the full list of ranking
-        # 2. remove unseen object categories from the full list
-        # 3. Take top k object categories as specified by one of the parameter
-        # 4. Format the filtering question with the scene graph class names
-        # 5. Format the filtering answer with the filtered ranking list
-        # 6. Format the selection question with the filtered ranking list (Need further confirmation)
-        # 7. Output filter_input_ids/filter_attention_mask/filter_length for the filtering question as well
-
-        # try:
-        # load a whole episode and each step within it
         step_path, episode_id = self.data[idx]
         try:
             step = self.load_step(step_path)
@@ -381,17 +330,10 @@ class ExploreDataset(Dataset):
             index = np.random.choice(self.indices)
             return self.__getitem__(index)
         episode = self.episodes[episode_id]
-        # try:
-        #     scene = self.scenes[episode["scene"]]
-        # except:
-        #     index = np.random.choice(self.indices)
-        #     return self.__getitem__(index)
+
         shuffle = self.random_permute and (self.split == "train")
-        # Jiachen TODO 1: load ranking
         ranking = self.candidate_rankings[episode["question"] + "_" + episode["scene"]]
-        # collections of features from egocentric view/action memory/scene graph/frontiers
         multi_src_features = []
-        # multi_src_positions = []
 
         with open(self.obj_json_map[episode["scene"]]) as f:
             obj_json = json.load(f)
@@ -452,7 +394,8 @@ class ExploreDataset(Dataset):
         snapshot_classes = []
         snapshot_positions = []
         snapshot_index = 0
-        # TODO: Fix this for prefiltering
+
+        # prefiltering TODO
         class2object = defaultdict(list)
         for i, rgb_id in enumerate(step["snapshot_features"].keys()):
             # No need to filter here (both scene_graph and snapshots objects from the json files)
@@ -480,17 +423,17 @@ class ExploreDataset(Dataset):
             except:
                 index = np.random.choice(self.indices)
                 return self.__getitem__(index)
-        # if self.add_positional_encodings:
-        #     object_features = [
-        #         sum_positional_encodings(
-        #             object_features[i].unsqueeze(0),
-        #             object_positions[i],
-        #             self.positional_encoding,
-        #             num_bins=self.num_bins,
-        #             coord_range=self.bounds,
-        #         ).squeeze(0)
-        #         for i in range(len(object_features))
-        #     ]
+        if self.add_positional_encodings:
+            snapshot_features = [
+                sum_positional_encodings(
+                    snapshot_features[i].unsqueeze(0),
+                    snapshot_positions[i],
+                    self.positional_encoding,
+                    num_bins=self.num_bins,
+                    coord_range=self.bounds,
+                ).squeeze(0)
+                for i in range(len(snapshot_features))
+            ]
         # print("original indices:", keep_indices)
         # print("seen categories:", object_classes)
 
@@ -505,14 +448,10 @@ class ExploreDataset(Dataset):
             index = np.random.choice(self.indices)
             return self.__getitem__(index)
 
+        # prefiltering TODO
         if self.prefiltering:
-            # 1. filter unseen object categories in ranking
             ranking = [cls for cls in ranking if cls in class2object.keys()]
-            # print("seen ranking:", ranking)
-            # 2. take top k object categories
             ranking = ranking[: self.top_k_categories]
-            # print(f"top {self.top_k_categories} ranking:", ranking)
-            # 3. reformulate the object indices, classes and features
             keep_indices = [
                 keep_indices[obj_idx]
                 for cls in ranking
@@ -524,21 +463,12 @@ class ExploreDataset(Dataset):
                 for cls in ranking
                 for obj_idx in class2object[cls]
             ]
-            # Note that if apply prefiltering, we may have #(objects) < object_index
-            # 4. reassign object_index = #(object)
             object_index = len(keep_indices)
 
-            # print("filtered indices:", keep_indices)
-            # print("filtered categories:", object_classes)
-        # Jiachen TODO: augment data by reindexing objects
         if shuffle:
             # shuffle the index if random_permute is True otherwise keep the original order
             random_snapshot_index = list(range(snapshot_index))
             np.random.shuffle(random_snapshot_index)
-            # print(object_index)
-            # print('random_object_index', random_object_index)
-            # print('indices before shuffle', keep_indices)
-            # print('classes before shuffle', object_classes)
             keep_indices = [keep_indices[r_idx] for r_idx in random_snapshot_index]
             snapshot_classes = [
                 snapshot_classes[r_idx] for r_idx in random_snapshot_index
@@ -546,9 +476,6 @@ class ExploreDataset(Dataset):
             snapshot_features = [
                 snapshot_features[r_idx] for r_idx in random_snapshot_index
             ]
-            # object_positions = [object_positions[r_idx] for r_idx in random_object_index]
-            # print('indices after shuffle', keep_indices)
-            # print('classes after shuffle', object_classes)
 
         text += "These are the snapshots:\n"
         for i, class_names in enumerate(snapshot_classes):
@@ -570,7 +497,6 @@ class ExploreDataset(Dataset):
         # shuffle frontier index
         # print("frontier before shuffle", [frontier['rgb_id'] for frontier in step["frontiers"]])
         frontier_index = list(range(len(step["frontiers"])))
-        # shuffle the index if random_permute is True otherwise keep the original order
         if shuffle:
             np.random.shuffle(frontier_index)
         # print("random_frontier_index", frontier_index)
@@ -585,7 +511,6 @@ class ExploreDataset(Dataset):
             frontier_positions = torch.tensor(
                 [step["frontier_positions"][idx] for idx in frontier_index]
             )
-            # TODO: figure out why this can be none?
             frontier_features = sum_positional_encodings(
                 frontier_features,
                 frontier_positions,
@@ -594,12 +519,9 @@ class ExploreDataset(Dataset):
                 coord_range=self.bounds,
             )
 
-        # print('frontier_text', frontier_text)
         text += frontier_text
-        # add frontier features
         multi_src_features.append(frontier_features)
-        # print("prediction before reformat", prediction)
-        # prepare prediction and answer
+
         prediction = np.concatenate(
             (
                 prediction[keep_indices],
@@ -611,7 +533,7 @@ class ExploreDataset(Dataset):
         # print("reformatted prediction", prediction)
         prediction = torch.tensor(prediction)
 
-        # prefiltering TODO: remove this in the prefiltering version
+        # prefiltering TODO: the assert might not be valid after prefiltering
         assert prediction.shape[0] == len(step["snapshot_features"]) + len(
             step["frontiers"]
         )
@@ -636,26 +558,17 @@ class ExploreDataset(Dataset):
             index = np.random.choice(self.indices)
             return self.__getitem__(index)
 
-        # default order: egocentric views -> action memory -> objects -> frontiers
         multi_src_features = [f for f in multi_src_features if f is not None]
         scene_feature = torch.cat(multi_src_features, dim=0)
 
         if len(scene_feature) > 45:
-            # take a random integer index
-            # random_idx = np.random.randint(0, len(self.data))
             self.too_many_objects_indices.add(idx)
             index = np.random.choice(self.indices)
             return self.__getitem__(index)
-            # return self.__getitem__(random_idx)
 
         step["scene_feature"] = scene_feature
-        # remove scene graph id --- remove this if we need to keep id
 
-        # make sure all things are included
-        # print("selection prompt", len(text))
-        # print(text)
         if self.max_length <= len(text):
-            # print(text)
             self.too_many_objects_indices.add(idx)
             index = np.random.choice(self.indices)
             return self.__getitem__(index)
@@ -712,14 +625,12 @@ class ExploreDataset(Dataset):
         # because sos token is added, the max_length should be +1?
         max_length = max(b.length for b in batch) + 1
         max_scene_length = max(b.scene_feature.shape[0] for b in batch)
-        # max_frontier_length = max(b.frontier_feature.shape[0] for b in batch)
 
         scene_feature = torch.zeros((len(batch), max_scene_length, 1024))
         scene_insert_loc = torch.zeros((len(batch), max_scene_length))
 
         for j, b in enumerate(batch):
             scene_feature[j, : b.scene_feature.shape[0]] = b.scene_feature
-            # frontier_feature[j, :b.frontier_feature.shape[0]] = b.frontier_feature
             scene_insert_loc[j, : b.scene_insert_loc.shape[0]] = b.scene_insert_loc
 
         if self.prefiltering:
@@ -735,7 +646,6 @@ class ExploreDataset(Dataset):
                 max_scene_length=torch.tensor(
                     [b.scene_feature.shape[0] for b in batch]
                 ),
-                # Jiachen TODO 7
                 filter_input_ids=torch.cat([b.filter_input_ids for b in batch])[
                     ..., :max_filter_length
                 ],
@@ -754,7 +664,6 @@ class ExploreDataset(Dataset):
             max_scene_length=torch.tensor([b.scene_feature.shape[0] for b in batch]),
         )
 
-    # split the dataset by episode id
     def split_index(self, test_ratio=0.3):
         test_num = int(test_ratio * len(self.episodes))
         test_episode = random.sample(range(len(self.episodes)), test_num)
@@ -769,61 +678,10 @@ class ExploreDataset(Dataset):
             for i in range(len(self.episodes))
             if int(self.episodes[i]["scene"].split("-")[0]) <= 700
         ]
-        # print("test episode", test_episode)
         train_index, test_index = [], []
-        # print(self.episode2step)
         for i in self.episode2step.keys():
             if i in test_episode:
                 test_index.extend(self.episode2step[i])
             if i in train_episode:
                 train_index.extend(self.episode2step[i])
         return train_index, test_index
-
-
-# if __name__ == "__main__":
-#     from transformers import AutoTokenizer
-#     from tqdm import tqdm
-
-#     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-
-#     additional_special_tokens = [SCENE_TOKEN]
-#     tokenizer.add_special_tokens(
-#         {"additional_special_tokens": additional_special_tokens}
-#     )
-#     dataset = ExploreDataset("../exploregraph_data", tokenizer, 2048)
-#     sampler = DistributedSampler(
-#         dataset, num_replicas=1, rank=0, shuffle=True, drop_last=False
-#     )
-#     dataloader = DataLoader(
-#         dataset,
-#         batch_size=4,
-#         pin_memory=True,
-#         num_workers=4,
-#         sampler=sampler,
-#         collate_fn=dataset.collate_wrapper,
-#     )
-
-#     for sample in tqdm(dataloader):
-#         print(sample)
-#         break
-
-# if __name__ == '__main__':
-#     # customize a tokenizer (Not sure how to add special tokens)
-#     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-#     tokenizer.add_special_tokens(
-#         {'additional_special_tokens':[
-#             SCENE_TOKEN,
-#             # FRONTIER_TOKEN,
-#             SELECT_TOKEN
-#         ]}
-#     )
-#     dataset = ExploreDataset('data',tokenizer,1024)
-
-#     # train test split
-#     train_index, test_index = dataset.split_index()
-#     train_dataset = Subset(dataset,train_index)
-#     test_dataset = Subset(dataset,test_index)
-
-#     train_loader = DataLoader(train_dataset, batch_size = 2, shuffle = True, collate_fn = dataset.collate_wrapper)
-#     batch = next(iter(train_loader))
-#     show_sample(batch)
